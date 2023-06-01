e32d18d401681ab55907ad5ecd11376ddb4b731e
marosandor <marosandor2001@gmail.com>
2023-06-01 12:22:22 +0200

    backend.js changed

--- backend.js
+++ backend.js
@@ -25,22 +25,22 @@ app.get("/", (req, res) => {
 });
 
 app.delete('/delNews', (req, res) => {
-    kapcsolat();
-    
-    connection.query("DELETE FROM news WHERE news_id = '" + req.body.id + "'",
-                     (err, rows, field) => {
-        if (err) throw err;
-        res.send({ message: 'Sikeres törlés' });
+  kapcsolat();
+
+  connection.query("DELETE FROM news WHERE news_id = '" + req.body.id + "'",
+    (err, rows, field) => {
+      if (err) throw err;
+      res.send({ message: 'Sikeres törlés' });
     });
-    
-    connection.end();
+
+  connection.end();
 });
 
 app.post("/setNews", (req, res) => {
   kapcsolat();
 
   connection.query(
-      "INSERT INTO news VALUES (NULL, '" + req.body.news_title + "', '" + req.body.news_description + "', '" + req.body.news_date + "')",
+    "INSERT INTO news VALUES (NULL, '" + req.body.news_title + "', '" + req.body.news_description + "', '" + req.body.news_date + "')",
     function (err, rows, fields) {
       if (err) console.log(err);
       else {
@@ -53,15 +53,15 @@ app.post("/setNews", (req, res) => {
 });
 
 app.delete('/delSchedules', (req, res) => {
-    kapcsolat();
-    
-    connection.query("DELETE FROM routes WHERE route_id = '" + req.body.id + "'",
-                     (err, rows, field) => {
-        if (err) throw err;
-        res.send({ message: 'Sikeres törlés' });
+  kapcsolat();
+
+  connection.query("DELETE FROM routes WHERE route_id = '" + req.body.id + "'",
+    (err, rows, field) => {
+      if (err) throw err;
+      res.send({ message: 'Sikeres törlés' });
     });
-    
-    connection.end();
+
+  connection.end();
 });
 
 // API végpont a stops tábla lekérdezéséhez
@@ -83,17 +83,17 @@ app.get("/stops", (req, res) => {
 
 // API végpont a routes tábla lekérdezéséhez
 app.get("/routes", (req, res) => {
-    kapcsolat();
-    
-    connection.query("SELECT routes_id, route_short_name, route_long_name, route_type, route_favorit FROM routes ORDER BY route_short_name", (err, rows, fields) => {
-        if (err) throw err;
-        
-        console.log("Lekérdezés megtörtént!");
-        res.json(rows);
-    }
-    );
-    
-    connection.end();
+  kapcsolat();
+
+  connection.query("SELECT routes_id, route_short_name, route_long_name, route_type, route_favorit FROM routes ORDER BY route_short_name", (err, rows, fields) => {
+    if (err) throw err;
+
+    console.log("Lekérdezés megtörtént!");
+    res.json(rows);
+  }
+  );
+
+  connection.end();
 });
 
 // Összes járat rövid illetve hosszú nevének lekérdezése járat id-vajl együtt
@@ -114,18 +114,18 @@ app.get("/jaratok", (req, res) => {
 });
 
 app.delete('/delSchedule', (req, res) => {
-    kapcsolat();
-    
-    const id = req.body.id + 1;
-    
-    connection.query("DELETE FROM trips WHERE route_id = " + id + "", (err, rows, field) => {
-        if (err) throw err;
-        
-        console.log("Törölve")
-        res.send('Törölve')
-    })
-    
-    connection.end()
+  kapcsolat();
+
+  const id = req.body.id + 1;
+
+  connection.query("DELETE FROM trips WHERE route_id = " + id + "", (err, rows, field) => {
+    if (err) throw err;
+
+    console.log("Törölve")
+    res.send('Törölve')
+  })
+
+  connection.end()
 })
 
 // Megállók kenevinek lekérdezése meállók id-val együtt
@@ -147,31 +147,31 @@ app.get("/megallok", (req, res) => {
 
 // Hírek/információk lekérése az adatbázisból
 app.get("/hirek", (req, res) => {
-    kapcsolat();
-    
-    connection.query("SELECT news_id, news_title, news_description, DATE_FORMAT(news_date, '%Y. %m. %d.') AS news_date  FROM `news` ORDER BY news_date DESC", (err, rows, fields) => {
-        if (err) throw err;
-        
-        console.log("Lekérés sikeres!");
-        res.send(rows);
-    });
-    connection.end();
+  kapcsolat();
+
+  connection.query("SELECT news_id, news_title, news_description, DATE_FORMAT(news_date, '%Y. %m. %d.') AS news_date  FROM `news` ORDER BY news_date DESC", (err, rows, fields) => {
+    if (err) throw err;
+
+    console.log("Lekérés sikeres!");
+    res.send(rows);
+  });
+  connection.end();
 });
 
 // Adott járatról vélemények lekérése
 app.post("/szuro_kereses", (req, res) => {
-   kapcsolat();
+  kapcsolat();
 
-   connection.query(
-     "SELECT opinion_id AS route_id, routes.route_short_name, comfort.opinions_desc AS comfort, ido.opinions_desc AS ido, crowd.opinions_desc AS crowd, opinions.opinion_comment FROM opinions INNER JOIN routes ON routes.route_id = opinions.opinion_route INNER JOIN opinions_desc AS comfort ON comfort.opinions_desc_id = opinions.opinion_comfort INNER JOIN opinions_desc AS ido ON ido.opinions_desc_id = opinions.opinion_time INNER JOIN opinions_desc AS crowd ON crowd.opinions_desc_id = opinions.opinion_crowd WHERE routes.route_short_name = '" + req.body.bemenet_1 + "'",
-     (err, rows, fields) => {
-       if (err) throw err;
+  connection.query(
+    "SELECT opinion_id AS route_id, routes.route_short_name, comfort.opinions_desc AS comfort, ido.opinions_desc AS ido, crowd.opinions_desc AS crowd, opinions.opinion_comment FROM opinions INNER JOIN routes ON routes.route_id = opinions.opinion_route INNER JOIN opinions_desc AS comfort ON comfort.opinions_desc_id = opinions.opinion_comfort INNER JOIN opinions_desc AS ido ON ido.opinions_desc_id = opinions.opinion_time INNER JOIN opinions_desc AS crowd ON crowd.opinions_desc_id = opinions.opinion_crowd WHERE routes.route_short_name = '" + req.body.bemenet_1 + "'",
+    (err, rows, fields) => {
+      if (err) throw err;
 
-       console.log("The solution is: ", rows);
-       res.send(rows);
-     }
-   );
-   connection.end();
+      console.log("The solution is: ", rows);
+      res.send(rows);
+    }
+  );
+  connection.end();
 });
 
 app.get("/kedvencek", (req, res) => {
@@ -191,7 +191,7 @@ app.get("/kedvencek", (req, res) => {
 
 app.post("/szures", (req, res) => {
   kapcsolat();
-    
+
   let parancs =
     "select * from routes where route_long_name like '%" +
     req.body.bevitel1 +
